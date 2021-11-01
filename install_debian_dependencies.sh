d14832c5905e6d4f4814f8c3e06e430158979a96
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2021-10-31 23:32:21 -0400

    [WIP] add dependencies for debian
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- install_debian_dependencies.sh
+++ install_debian_dependencies.sh
@@ -1,2 +1,7 @@
-wget http://steinerdatenbank.de/software/mbrola3.0.1h_armhf.deb
-sudo dpkg -i mbrola3.0.1h_armhf.deb
+sudo apt update
+sudo apt install espeak -y
+mkdir /home/pi/espeak
+cd /home/pi/espeak
+wget https://raspberry-pi.fr/download/espeak/mbrola3.0.1h_armhf.deb -O mbrola.deb
+sudo dpkg -i mbrola.deb
+sudo apt install mbrola-fr1 -y
