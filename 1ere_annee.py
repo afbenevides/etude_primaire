52328537d0c772fa4e91975cdf6dfd063d62e877
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2021-11-03 18:49:10 -0400

    [ADD] ajout activite dictee
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- 1ere_annee.py
+++ 1ere_annee.py
@@ -51,11 +51,24 @@ class PremiereAnnee:
         else:
             return False
 
-    def donner_reponse(self, type_reponse, reponse):
+    def lire_reponse(self, type_reponse, reponse):
         touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
         if self.valider_action(touche):
             self.dire("le {0} était".format(type_reponse))
             self.dire(reponse)
+            touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
+            if self.valider_action(touche):
+                return True
+            else:
+                return self.quitter()
+        else:
+            return self.quitter()
+
+    def afficher_reponse(self, type_reponse, reponse):
+        touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
+        if self.valider_action(touche):
+            self.dire("le {0} {1} s\\\'écrit comme ceci:".format(type_reponse, reponse))
+            print("====> \033[92m {0} \033[0m <====".format(reponse))
             touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
             if self.valider_action(touche):
                 return True
@@ -213,19 +226,7 @@ class MotsEtiquettes(PremiereAnnee):
         mot = self.choix_hasard(self.liste_de_mots, 0, len(self.liste_de_mots) - 1)
         self.question("{0} quel est ce mot ?".format(self.nom))
         print("====> \033[92m {0} \033[0m <====".format(mot))
-        return self.donner_reponse("mot", mot)
-        #
-        # touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
-        # if self.valider_action(touche):
-        #     self.dire("le mot était")
-        #     self.dire(mot)
-        #     touche = self.question_reponse("\"Enter\" pour continuer ou \"q\" pour quittter")
-        #     if self.valider_action(touche):
-        #         return True
-        #     else:
-        #         return self.quitter()
-        # else:
-        #     return self.quitter()
+        return self.lire_reponse("mot", mot)
 
 class Nombres_aleatoires(PremiereAnnee):
     def __init__(self, nom):
@@ -248,8 +249,163 @@ class Nombres_aleatoires(PremiereAnnee):
             nombre = random.randint(minimum, maximum)
             self.question("{0} quel est ce nombre ?".format(self.nom))
             print("====> \033[92m {0} \033[0m <====".format(nombre))
-            sortie = self.donner_reponse("nombre", str(nombre))
+            sortie = self.lire_reponse("nombre", str(nombre))
 
+class Dictee(PremiereAnnee):
+    def __init__(self, nom):
+        super().__init__()
+        self.nom = nom
+        self.dictionnaire_list = dict()
+
+        #####################
+        self.liste_A = ["à",
+                        "en",
+                        "soi",
+                        "par",
+                        "avec",
+                        "dans",
+                        "vous",
+                        "plus",
+                        "et",
+                        "ta"]
+        self.liste_B = ["aller",
+                        "elle",
+                        "pour",
+                        "autre",
+                        "des",
+                        "bien",
+                        "si",
+                        "sur",
+                        "aux",
+                        "lui"]
+        self.liste_C = ["se",
+                        "cette",
+                        "moi",
+                        "où",
+                        "jour",
+                        "dire",
+                        "le",
+                        "tous",
+                        "ne",
+                        "qui"]
+        self.liste_D = ["pas",
+                        "leur",
+                        "un",
+                        "que",
+                        "être",
+                        "tu",
+                        "pomme",
+                        "ils",
+                        "du",
+                        "mon"]
+        self.liste_E = ["elles",
+                        "de",
+                        "t'",
+                        "tout",
+                        "vouloir",
+                        "vos",
+                        "sans",
+                        "ces",
+                        "au",
+                        "la"]
+        self.liste_F = ["toi",
+                        "faire",
+                        "ce",
+                        "il",
+                        "son",
+                        "uns",
+                        "y",
+                        "je",
+                        "d'",
+                        "mais"]
+        self.liste_G = ["donner",
+                        "prendre",
+                        "ma",
+                        "une",
+                        "par",
+                        "sa",
+                        "te",
+                        "n'est",
+                        "qu'il",
+                        "ses"]
+        self.liste_H = ["avoir",
+                        "tes",
+                        "me",
+                        "ton",
+                        "c'est",
+                        "les",
+                        "j'ai",
+                        "cet",
+                        "votre",
+                        "venir"]
+        self.liste_I = ["l'",
+                        "savoir",
+                        "m'",
+                        "pouvoir",
+                        "unes",
+                        "leurs",
+                        "mes"]
+
+        self.dictionnaire_list["A"] = self.liste_A
+        self.dictionnaire_list["B"] = self.liste_B
+        self.dictionnaire_list["C"] = self.liste_C
+        self.dictionnaire_list["D"] = self.liste_D
+        self.dictionnaire_list["E"] = self.liste_E
+        self.dictionnaire_list["F"] = self.liste_F
+        self.dictionnaire_list["G"] = self.liste_G
+        self.dictionnaire_list["H"] = self.liste_H
+        self.dictionnaire_list["I"] = self.liste_I
+        print("=======================================================================================================")
+        parole = "Les listes disponibles sont:"
+        print(parole)
+        self.dire(parole)
+        for key in self.dictionnaire_list:
+            print("Liste", key, " --> ", self.dictionnaire_list[key])
+        print("=======================================================================================================")
+        #####################
+
+        question = "Quelles listes veux tu pratiquer?"
+        listes = self.question_reponse(question)
+
+        self.dire("Les listes choisies sont les")
+
+        self.valids = list()
+        for character in listes.upper():
+            if character.isalpha():
+                if character in self.dictionnaire_list.keys():
+                    self.valids.append(character)
+                else:
+                    print("La liste", character, "n\'existe pas")
+                    exit()
+        for each in self.valids:
+            print(str(each))
+            self.dire(str(each))
+
+        self.liste_de_mots = list()
+        for each in self.valids:
+            for word in self.dictionnaire_list[each]:
+                self.liste_de_mots.append(word)
+
+        maximum = len(self.liste_de_mots)
+        nombre_mots = self.nom + ", tu as choisi " + str(maximum) + " mots"
+        print(nombre_mots)
+        self.dire(nombre_mots)
+        time.sleep(1)
+
+        sortie = True
+        while sortie:
+            sortie = self.lire_un_mot_dictee()
+
+    def lire_un_mot_dictee(self):
+        mot = self.choix_hasard(self.liste_de_mots, 0, len(self.liste_de_mots) - 1)
+        self.question("{0} écrit le mot".format(self.nom))
+        time.sleep(0.5)
+        self.dire(mot)
+        time.sleep(5)
+        self.dire(mot)
+        time.sleep(5)
+        self.dire(mot)
+        return self.afficher_reponse("mot", mot)
 
 #programme = MotsEtiquettes()
 programme = PremiereAnnee()
@@ -257,6 +413,7 @@ programme.demander_nom()
 
 print("A - Mots étiquettes")
 print("B - Nombres Aléatoires")
+print("C - Dictée")
 question = "Quelle activité veux-tu faire? "
 reponse = programme.question_reponse(question)
 
@@ -264,6 +421,8 @@ if reponse.upper() == "A":
     MotsEtiquettes(programme.nom)
 elif reponse.upper() == "B":
     Nombres_aleatoires(programme.nom)
+elif reponse.upper() == "C":
+    Dictee(programme.nom)
 else:
     question = ("Désolé, votre choix est non valide")
     programme.question(question)
